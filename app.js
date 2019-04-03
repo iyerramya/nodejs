685378af702adb61220af778fb8e38f9f0f9e38e
iyerramya <43103509+iyerramya@users.noreply.github.com>
2019-04-03 13:09:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -54,8 +54,16 @@ var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
 
-var db_host = (process.env.mongodb_host || "localhost" ) ;
+/*var db_host = (process.env.mongodb_host || "localhost" ) ;
 var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_user = (process.env.mongodb_user || "cmpe281" ) ;
+var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
+var db_name = (process.env.mongodb_name || "test" ) ;
+mongodb://<dbuser>:<dbpassword>@ds147125.mlab.com:47125/test1
+*/
+
+var db_host = (process.env.mongodb_host || "ds147125.mlab.com" ) ;
+var db_port = (process.env.mongodb_port || "47125" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
 var db_name = (process.env.mongodb_name || "test" ) ;
